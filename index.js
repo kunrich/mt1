c5195abd4ac6cc4deecf75a318171f728e658692
kunrich <kunrich202202@gmail.com>
2021-01-26 00:26:10 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.get("/",(req,res)=>{
 var a1 = 'https://player.marimo.life/demo/?key=19qk0qWgZpKSoMujx2WfxK-WzG7Q0N-Y&vid=18fjveikp2HO6Kl5w82v1MjX25vSwtx869Pcl8afmYrV0KvSz6-qsaXJpLHusM-fieKcjNewjaaR2OOutcHBhurC3qLDomOxttWa58ir0t3emqOQytm5manmh4Cuwqyuuuzgr6TA5q_Gtb-HmsGExKWRs97G7J_RwZi7t7rRvIKeuoiNr6il1tXVr6TVm-KfyeLImtDbnH2wkXmgrLjQuJ-Z1Li51L-frdOfo8CUoteq';
 request(a1, function (error, response, body) {
 if (!error && response.statusCode == 200) {
-console.log("ok request 2...");
 var t1='<div id="player"><script>contents';
 var t2=';</script></div>';
 var b1=body.substring(body.search(t1),body.search(t2)).substr(t1.length+2);
